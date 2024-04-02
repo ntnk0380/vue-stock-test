c38ae23b6bd645f447b39fabc32b54e69f42721d
ntnk0380 <88926140+ntnk0380@users.noreply.github.com>
2024-04-03 00:44:34 +0700

    fix ( router index.js)
    redirect path '/' to '/stock'

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/',  redirect: '/stock' },
     { path: '/stock', name: 'stock-page', component: () => import('../views/Stock.vue')},
     { path: '/stock/add', name: 'add-stock-page', component: () => import('../views/AddStock.vue')},
     { path: '/:pathMatch(.*)*', name: 'not-found-page', component: () => import('../views/PageNotFound.vue')},
+    // { path: '/:pathMatch(.*)*', name: 'not-found-page', component: () => import('../views/PageNotFound.vue')},
+
     // { path: '/', name: 'home', component: () => import('../views/HomeView.vue')},
     // { path: '/about', name: 'about', component: () => import('../views/AboutView.vue')}
   ]
